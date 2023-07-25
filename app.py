b1492df38e79089fbd6c8e161544e76aab9044bc
prahalad.an <prahaladanand6@gmail.com>
2023-07-25 11:06:46 -0500

    Create app.py, style.css, index.js, homepage.html

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template, jsonify
+import yfinance as yf
+
+app = Flask(__name__)
+
+@app.route("/quote")
+def display_quote():
+	symbol = request.args.get('symbol', default="AAPL")
+	quote = yf.Ticker(symbol)
+	return jsonify(quote.info)
+
+@app.route("/history")
+def display_history():
+	symbol = request.args.get('symbol', default="AAPL")
+	period = request.args.get('period', default="1y")
+	interval = request.args.get('interval', default="1mo")
+
+	quote = yf.Ticker(symbol)	
+	hist = quote.history(period=period, interval=interval)
+	data = hist.to_json()
+	return data
+
+@app.route("/")
+def home():
+    return render_template("homepage.html")
+
+if __name__ == "__main__":
+	app.run(debug=True)
